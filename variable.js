830bb4e0108dc290d877efe6e018b4a0bc19792b
mrabimanyu <muhammad.abimanyu74@gmail.com>
2022-12-24 23:14:41 +0700

    added new file and modified

---
+++ variable.js
@@ -0,0 +1,14 @@
+//const name = " halo raihan";
+//name = "abimanyu";
+//console.log(name);
+//const student = {
+  //  name: "abimanyu",
+    //class: 2
+//}
+
+//student.faculty = "sainteks";
+//console.log(student);
+
+const students = ['ani','anu','ane'];
+students.push('eka');
+console.log(students);
