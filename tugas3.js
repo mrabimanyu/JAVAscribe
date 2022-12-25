76b102687733161b5f7d54bb70669f74f0888fb8
mrabimanyu <muhammad.abimanyu74@gmail.com>
2022-12-25 21:47:30 +0700

    added new file

---
+++ tugas3.js
@@ -0,0 +1,11 @@
+"use strict"
+var length = 16; //number
+var lastName = "John"; //string
+const x = {
+  firstName: "John"
+  lastName: "Jo"
+  age: 19
+}; //object
+console.log(`data ini : ${length} --> dengan type data ${typeof length}`);
+console.log(`data ini : ${lastName} --> dengan type data ${typeof lastName}`);
+console.log(`data ini : ${x} --> dengan type data ${typeof x}`);
